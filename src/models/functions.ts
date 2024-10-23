ddffa855555e6f43bf10efbf08df00a01a2c839f
stello <aramastelian@gmail.com>
2024-10-23 17:13:50 +0200

    Flyttat funktionerna till functions.ts

---
+++ src/models/functions.ts
@@ -0,0 +1,76 @@
+export function updateTodoList(data: {task: string; isDone: boolean}[], defaultTasks: {task: string; isDone: boolean}[], ol: HTMLElement | null) {
+    if (ol) {
+        ol.innerHTML = "";
+        for (let i=0; i < data.length; i++) {
+            let li = document.createElement("li");
+            li.innerHTML = data[i].task;
+
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = data[i].isDone;
+            checkbox.addEventListener("change", function() {
+                toggleDone(i, checkbox.checked, data, defaultTasks, ol);
+            });
+
+            li.insertBefore(checkbox, li.firstChild);
+
+            if (data[i].isDone) {
+                let deliteButton = document.createElement("button");
+                deliteButton.innerHTML = "Radera";
+                deliteButton.addEventListener("click", function() {
+                    deleteTask(i, data, defaultTasks, ol);
+                });
+                li.appendChild(deliteButton);
+            }
+
+            let moveUpButton = document.createElement("button");
+            moveUpButton.innerHTML = "\u2191";
+            moveUpButton.addEventListener("click", function () {
+                moveTaskUp(i, data, defaultTasks, ol);
+            });
+
+            let moveDownButton = document.createElement("button");
+            moveDownButton.innerHTML = "\u2193";
+            moveDownButton.addEventListener("click", function () {
+                moveTaskDown(i, data, defaultTasks, ol);
+            });
+            li.appendChild(moveUpButton);
+            li.appendChild(moveDownButton);
+    
+            ol?.appendChild(li);
+        }
+    }
+}
+
+export function toggleDone(index: number, isDone: boolean, data: {task: string; isDone: boolean}[], defaultTasks: {task: string; isDone: boolean}[], ol: HTMLElement | null) {
+    data[index].isDone = isDone;
+
+    const dynamicTasks = data.slice(defaultTasks.length);
+    localStorage.setItem("tasks", JSON.stringify(dynamicTasks));
+    updateTodoList(data, defaultTasks, ol);
+}
+
+export function deleteTask(index: number, data: {task: string; isDone: boolean}[], defaultTasks: {task: string; isDone: boolean}[], ol: HTMLElement | null) {
+    data.splice(index, 1);
+    const dynamicTasks = data.slice(defaultTasks.length);
+    localStorage.setItem("tasks", JSON.stringify(dynamicTasks));
+    updateTodoList(data, defaultTasks, ol);
+}
+
+export function moveTaskUp(index: number, data: {task: string; isDone: boolean}[], defaultTasks: {task: string; isDone: boolean}[], ol: HTMLElement | null) {
+    if (index > 0) {
+        [data[index - 1], data[index]] = [data[index ], data[index - 1]];
+        const dynamicTasks = data.slice(defaultTasks.length);
+        localStorage.setItem("tasks", JSON.stringify(dynamicTasks));
+        updateTodoList(data, defaultTasks, ol);
+    }
+}
+
+export function moveTaskDown(index: number, data: {task: string; isDone: boolean}[], defaultTasks: {task: string; isDone: boolean}[], ol: HTMLElement | null) {
+    if (index < data.length -1) {
+        [data[index], data[index + 1]] = [data[index +1], data[index]];
+        const dynamicTasks = data.slice(defaultTasks.length);
+        localStorage.setItem("tasks", JSON.stringify(dynamicTasks));
+        updateTodoList(data, defaultTasks, ol);
+    }
+}
